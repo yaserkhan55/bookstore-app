8976c805193eaa0aa0450128bbf2d6d6ea539524
Yaser Khan <yaserahmed9657@gmail.com>
2025-11-04 02:06:26 -0800

    fix: link backend to vercel serverless api

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,6 +1,3 @@
 // Backend/api/index.js
 import app from "../index.js";
-import serverless from "serverless-http";
-
-export const handler = serverless(app);
-export default handler;
+export default app;
