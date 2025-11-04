fe61046642d068759ddcc87bf2a28e54b92d37fa
Yaser Khan <yaserahmed9657@gmail.com>
2025-11-04 01:55:04 -0800

    fix: add serverless backend api entrypoint for Vercel

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -2,6 +2,5 @@
 import app from "../index.js";
 import serverless from "serverless-http";
 
-// Wrap your Express app in a serverless function handler
 export const handler = serverless(app);
 export default handler;
